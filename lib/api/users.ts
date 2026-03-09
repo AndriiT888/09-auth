319871136f7b70e21a3b5a16038f82bb0daaa156
Andrii888 <aotk888@gmail.com>
2026-03-09 21:59:02 +0200

    fix-dz-913

--- lib/api/users.ts
+++ lib/api/users.ts
@@ -1,13 +1,13 @@
 import type { AxiosResponse } from "axios";
 import api from "./axios";
-import type { UpdateUserPayload, User } from "@/types/user";
+import type {User } from "@/types/user";
 
 export const getMe = async (): Promise<User> => {
   const res: AxiosResponse<User> = await api.get("/users/me");
   return res.data;
 };
 
-export const updateMe = async (payload: UpdateUserPayload): Promise<User> => {
-  const res: AxiosResponse<User> = await api.patch("/users/me", payload);
-  return res.data;
-};
+// export const updateMe = async (payload: UpdateUserPayload): Promise<User> => {
+//   const res: AxiosResponse<User> = await api.patch("/users/me", payload);
+//   return res.data;
+// };
